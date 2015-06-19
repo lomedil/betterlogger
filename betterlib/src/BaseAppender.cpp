fa4dc58771b8486a8c6a9ef4ec84bd753724bb50
Jairo Sansegundo <jairo.sansegundo@gmail.com>
2015-06-19 21:42:06 +0200

    Convinient class for appenders with formatter

---
+++ betterlib/src/BaseAppender.cpp
@@ -0,0 +1,31 @@
+#include <appenders/BaseAppender.h>
+
+BEGIN_NS_BETTER_APPENDERS
+
+
+BaseAppender::BaseAppender() :
+    m_pFormatter(0)
+{
+
+}
+
+BaseAppender::~BaseAppender()
+{  }
+
+bool BaseAppender::hasFormatter() const
+{
+    return m_pFormatter != 0;
+}
+
+IFormatter *BaseAppender::formatter()
+{
+    return m_pFormatter;
+}
+
+void BaseAppender::installFormatter(IFormatter *formatter)
+{
+    m_pFormatter = formatter;
+}
+
+
+END_NS_BETTER_APPENDERS

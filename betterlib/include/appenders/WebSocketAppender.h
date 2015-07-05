ce33b279cc4deb1a198edc94c9de850600d4fffb
Jairo Sansegundo <jairo.sansegundo@gmail.com>
2015-07-06 00:39:12 +0200

    Send entries as bundle each second

--- betterlib/include/appenders/WebSocketAppender.h
+++ betterlib/include/appenders/WebSocketAppender.h
@@ -19,10 +19,21 @@ public:
 private:
     QWebSocketServer        *m_pServer;
     QList<QWebSocket*>      m_clients;
+    BundleFormatter         m_nextBundle;
+    QTimer                  *m_pTimer;
+    QMutex                  *m_pMutex;
 
 private slots:
     void onNewConnection();
     void removeClient();
+    void pushBundle();
+
+protected:
+    void onFormatterChanged(); // override
+
+private:
+    void resetBundleFormatter();
+    void pushMessage(const QString &message);
 
 
     // ILogAppender interface
